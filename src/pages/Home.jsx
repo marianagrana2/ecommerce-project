dcf6ab26d42376ac4afdf6c3f85b1dc1ecafa847
marianagrana2 <mariana.grana2@gmail.com>
2023-08-26 15:45:54 -0600

    Disenio Login & menu despegable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,12 @@
 import { useState, useEffect } from 'react' // Importarlos para poder utilizarlos para llamar a la API
 const Home = () => {
   const [products, setProducts] = useState([]) // Donde se va almacenar los datos de la API
-  // const search = useState('')
   useEffect(() => { // Llamada a la API
     fetch('https://ecommerce-json-jwt.onrender.com/items')
       .then(response => response.json())
       .then(data => setProducts(data))
       .catch(error => console.error(error))
   }, [])
-
-  /* const filteredProducts = products.filter(product => {
-    return product.name.toLowerCase().includes(search.toLocalLowerCase())
-  }) */
   return (
     <>
       <div className='container'>
